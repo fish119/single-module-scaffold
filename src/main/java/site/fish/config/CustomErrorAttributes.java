672905c20026a9e32812220507f969ee77e1d3c8
郭绪龙 <gxl@ycim.tech>
2021-02-03 14:42:50 +0800

    add: Validation

--- src/main/java/site/fish/config/CustomErrorAttributes.java
+++ src/main/java/site/fish/config/CustomErrorAttributes.java
@@ -3,6 +3,9 @@ package site.fish.config;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDate;
@@ -10,6 +13,8 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,6 +39,18 @@ public class CustomErrorAttributes extends DefaultErrorAttributes {
         } else {
             errorAttributes.put("timestamp", formatter.format(((Date) timestamp).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()));
         }
+        /*
+        if (!ObjectUtils.isEmpty(errorAttributes.get("errors"))) {
+            Map<String, String> errors = new HashMap<>(2);
+            ((List<ObjectError>) errorAttributes.get("errors"))
+                    .forEach((error) -> {
+                        String fieldName = ((FieldError) error).getField();
+                        String errorMessage = error.getDefaultMessage();
+                        errors.put(fieldName, errorMessage);
+                    });
+            errorAttributes.put("errors",errors);
+        }
+         */
         return errorAttributes;
     }
 }
--- src/main/java/site/fish/config/WebExceptionHandler.java
+++ src/main/java/site/fish/config/WebExceptionHandler.java
@@ -1,17 +1,26 @@
 package site.fish.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Description: [统一异常处理类]
@@ -23,17 +32,18 @@ import java.io.IOException;
  * @date : 2021/2/1 11:34
  */
 @ControllerAdvice
-public class WebExceptionHandler extends ResponseEntityExceptionHandler {
+public class WebExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(Exception.class)
     public void unKnowExceptionHandler(Exception ex, HttpServletResponse response, HttpServletRequest request) throws IOException {
-        logger.error("500:" + ex.getClass().getName() + ex.getLocalizedMessage() + ex.getMessage() + "|||Url=" + request.getRequestURI());
+        log.error("500:" + ex.getClass().getName() + ex.getLocalizedMessage() + ex.getMessage() + "|||Url=" + request.getRequestURI());
         response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "服务器异常");
     }
 
     @ExceptionHandler({AuthenticationException.class, BadCredentialsException.class})
     public void authenticationExceptionHandler(Exception authException, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        logger.error("401:" + authException.getMessage() + "|||Url=" + request.getRequestURI());
+        log.error("401:" + authException.getMessage() + "|||Url=" + request.getRequestURI());
         String msg = ObjectUtils.isEmpty(authException.getLocalizedMessage()) ? ExceptionMessage.BAD_CREDENTIALS : authException.getLocalizedMessage();
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
                 "Full authentication is required to access this resource".equals(authException.getLocalizedMessage()) ?
@@ -42,8 +52,30 @@ public class WebExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
     public void unauthorizedExceptionHandler(Exception accessDeniedException, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        logger.error("403:" + accessDeniedException.getLocalizedMessage() + "|||Url=" + request.getRequestURI());
+        log.error("403:" + accessDeniedException.getLocalizedMessage() + "|||Url=" + request.getRequestURI());
         String msg = ObjectUtils.isEmpty(accessDeniedException.getLocalizedMessage()) ? ExceptionMessage.FORBIDDEN : accessDeniedException.getLocalizedMessage();
         response.sendError(HttpServletResponse.SC_FORBIDDEN, msg);
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public Map<String, Object> handleValidationExceptions(BindException ex, HttpServletRequest request, HttpServletResponse response) {
+        log.error("400:" + ex.getLocalizedMessage() + "|||Url=" + request.getRequestURI());
+        Map<String, Object> result = new HashMap<>(6);
+        Map<String, String> errors = new HashMap<>(2);
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        result.put("timestamp", formatter.format(LocalDateTime.now()));
+        result.put("status", HttpServletResponse.SC_BAD_REQUEST);
+        result.put("message", "参数校验失败");
+        result.put("errors", errors);
+        result.put("path", request.getRequestURI());
+        return result;
     }
 }
