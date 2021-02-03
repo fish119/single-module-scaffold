4392e3f40829c82d7871b35218d5cc9a5d02157c
郭绪龙 <gxl@ycim.tech>
2021-02-03 12:32:54 +0800

    fix : Exception Handler

--- src/main/java/site/fish/config/WebExceptionHandler.java
+++ src/main/java/site/fish/config/WebExceptionHandler.java
@@ -1,8 +1,10 @@
 package site.fish.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -22,11 +24,26 @@ import java.io.IOException;
  */
 @ControllerAdvice
 public class WebExceptionHandler extends ResponseEntityExceptionHandler {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-//    @ExceptionHandler(Exception.class)
-//    public void unKnowExceptionHandler(Exception ex, HttpServletResponse response, HttpServletRequest request) throws IOException {
-//        logger.error("500:" + ex.getClass().getName() + ex.getLocalizedMessage() + ex.getMessage() + "|||Url=" + request.getRequestURI());
-//        response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "服务器异常");
-//    }
+    @ExceptionHandler(Exception.class)
+    public void unKnowExceptionHandler(Exception ex, HttpServletResponse response, HttpServletRequest request) throws IOException {
+        logger.error("500:" + ex.getClass().getName() + ex.getLocalizedMessage() + ex.getMessage() + "|||Url=" + request.getRequestURI());
+        response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "服务器异常");
+    }
+
+    @ExceptionHandler({AuthenticationException.class, BadCredentialsException.class})
+    public void authenticationExceptionHandler(Exception authException, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("401:" + authException.getMessage() + "|||Url=" + request.getRequestURI());
+        String msg = ObjectUtils.isEmpty(authException.getLocalizedMessage()) ? ExceptionMessage.BAD_CREDENTIALS : authException.getLocalizedMessage();
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                "Full authentication is required to access this resource".equals(authException.getLocalizedMessage()) ?
+                        ExceptionMessage.BAD_CREDENTIALS : msg);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public void unauthorizedExceptionHandler(Exception accessDeniedException, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("403:" + accessDeniedException.getLocalizedMessage() + "|||Url=" + request.getRequestURI());
+        String msg = ObjectUtils.isEmpty(accessDeniedException.getLocalizedMessage()) ? ExceptionMessage.FORBIDDEN : accessDeniedException.getLocalizedMessage();
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, msg);
+    }
 }
